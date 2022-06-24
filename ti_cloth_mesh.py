52067c56abf5f5079fdd6bc19cf6a55ec0284ae0
Dongping Li <lidongping83@gmail.com>
2022-06-24 15:27:10 +0800

    topology done

--- ti_cloth_mesh.py
+++ ti_cloth_mesh.py
@@ -8,12 +8,39 @@ class ClothMesh(BaseMesh):
         super().__init__(mesh_obj)
         self.total_mass = sim_param.total_mass
         self.verts_mass = ti.field(ti.f32, self.n_verts)
-        self.edges_length_rest = ti.field(ti.f32, self.n_edges)
-        self.compute_verts_mass()
-        self.compute_edge_length_rest()
+        self._compute_verts_mass()
+        data_indices_coo = self._calc_indices_coo(self.tris.to_numpy())
+        self.n_indices_coo = len(data_indices_coo)
+        self.indices_coo = ti.Vector.field(2, ti.i32, self.n_indices_coo)
+        self.indices_coo.from_numpy(np.array(data_indices_coo, dtype = np.int32))
+        self.indices_csr_ptr = ti.field(ti.i32, self.n_verts + 1)
+        self._compute_indices_csr()
+        self.indices_diag = ti.field(ti.i32, self.n_verts)
+        self._compute_indices_diag()
+        self._tmp_count_e_of_t = ti.field(ti.i32, self.n_indices_coo)
+        self.indices_tri_of_edge = ti.field(ti.i32, (self.n_indices_coo, 2))
+        self._compute_indices_tri_of_edge()
+
+    # compute via python, since taichi does not support reduction ops
+    def _calc_indices_coo(self, data_triangles):
+        data_indices_coo = []
+        for i in range(len(data_triangles)):
+            t = data_triangles[i]
+            data_indices_coo.append((t[0], t[0]))
+            data_indices_coo.append((t[0], t[1]))
+            data_indices_coo.append((t[0], t[2]))
+            data_indices_coo.append((t[1], t[0]))
+            data_indices_coo.append((t[1], t[1]))
+            data_indices_coo.append((t[1], t[2]))
+            data_indices_coo.append((t[2], t[0]))
+            data_indices_coo.append((t[2], t[1]))
+            data_indices_coo.append((t[2], t[2]))
+        data_indices_coo.sort()
+        data_indices_coo = self._unique(data_indices_coo)
+        return data_indices_coo
 
     @ti.kernel
-    def compute_verts_mass(self):
+    def _compute_verts_mass(self):
         for i in self.verts:
             self.verts_mass[i] = 0.0
         for i in self.tris:
@@ -31,10 +58,61 @@ class ClothMesh(BaseMesh):
         mass_scale = self.total_mass / sum_mass
         for i in self.verts:
             self.verts_mass[i] *= mass_scale
+            
+    @ti.kernel
+    def _compute_indices_csr(self):
+        for i in self.indices_csr_ptr:
+            self.indices_csr_ptr[i] = 0
+        for i in self.indices_coo:
+            if i > 0:
+                e_prev = self.indices_coo[i-1]
+                e = self.indices_coo[i]
+                if e[0] != e_prev[0]:
+                    self.indices_csr_ptr[e[0]] = i
+        self.indices_csr_ptr[self.n_verts] = self.n_indices_coo
+        
+    @ti.kernel
+    def _compute_indices_diag(self):
+        for i in self.indices_diag:
+            self.indices_diag[i] = 0
+        for i in self.indices_coo:
+            e = self.indices_coo[i]
+            if e[0] == e[1]:
+                self.indices_diag[e[0]] = i
 
     @ti.kernel
-    def compute_edge_length_rest(self):
-        for i in self.edges:
-            e = self.edges[i]
-            dist = (self.verts[e[0]] - self.verts[e[1]]).norm()
-            self.edges_length_rest[i] = dist
+    def _compute_indices_tri_of_edge(self):
+        for i in self.indices_coo:
+            self.indices_tri_of_edge[i,0] = -1
+            self.indices_tri_of_edge[i,1] = -1
+            self._tmp_count_e_of_t[i] = 0
+        for i in self.tris:
+            t = self.tris[i]
+            index = self._find_coo_index(t[0], t[1])
+            if index >= 0:
+                self.indices_tri_of_edge[index, ti.atomic_add(self._tmp_count_e_of_t[index], 1)] = i
+            index = self._find_coo_index(t[1], t[0])
+            if index >= 0:
+                self.indices_tri_of_edge[index, ti.atomic_add(self._tmp_count_e_of_t[index], 1)] = i
+            index = self._find_coo_index(t[0], t[2])
+            if index >= 0:
+                self.indices_tri_of_edge[index, ti.atomic_add(self._tmp_count_e_of_t[index], 1)] = i
+            index = self._find_coo_index(t[2], t[0])
+            if index >= 0:
+                self.indices_tri_of_edge[index, ti.atomic_add(self._tmp_count_e_of_t[index], 1)] = i
+            index = self._find_coo_index(t[1], t[2])
+            if index >= 0:
+                self.indices_tri_of_edge[index, ti.atomic_add(self._tmp_count_e_of_t[index], 1)] = i
+            index = self._find_coo_index(t[2], t[1])
+            if index >= 0:
+                self.indices_tri_of_edge[index, ti.atomic_add(self._tmp_count_e_of_t[index], 1)] = i
+
+    @ti.func
+    def _find_coo_index(self, row, col):
+        begin = self.indices_csr_ptr[row]
+        end = self.indices_csr_ptr[row+1]
+        index = -1
+        for pos in range(begin, end):
+            if self.indices_coo[pos][1] == col:
+                index = pos
+        return index
--- ti_pbd_solver.py
+++ ti_pbd_solver.py
@@ -9,6 +9,23 @@ class PbdSolver:
         self.body = body_mesh
         self.sim_param = sim_param
         
+    #     self.mat_A_diag = ti.field(ti.f32, self.cloth.n_verts)
+    #     self.mat_A_other = ti.field(ti.f32, self.cloth.n_edges)
+    #     self.vec_B = ti.Vector.field(3, ti.f32, self.cloth.n_verts)
+    #     self.vec_X = ti.Vector.field(3, ti.f32, self.cloth.n_verts)
+        
+    # @ti.kernel
+    # def compute_A(self):
+    #     for i in self.mat_A_diag:
+    #         self.mat_A_diag[i] = 0.0
+    #     for i in self.cloth.edges:
+    #         e = self.cloth.edges[i]
+            
+    #         # handle spring length
+    #         self.mat_A_other[i] = -self.sim_param.spring_stiffness
+    #         self.mat_A_diag[e[0]] += self.sim_param.spring_stiffness * 0.5
+    #         self.mat_A_diag[e[1]] += self.sim_param.spring_stiffness * 0.5
+        
 
     def update(self, dt = 0.033):
         pass
